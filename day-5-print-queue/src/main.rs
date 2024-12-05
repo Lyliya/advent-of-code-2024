f5fa641dac7264efac3d8506d301f2400e16cd8e
Guillaume Guerin <guillaume.guerin@playplay.com>
2024-12-05 13:56:47 +0100

    add: day 5 step 2

--- day-5-print-queue/src/main.rs
+++ day-5-print-queue/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::collections::HashMap;
+use std::cmp::Ordering;
 
 #[derive(Debug)]
 struct PageOrder {
@@ -31,14 +32,13 @@ fn is_correct_order(page_map: &HashMap<usize, PageOrder>, pages_numbers: &Vec<us
     return true;
 }
 
-fn step1(input: &String) {
+fn step2(input: &String) {
     let lines: Vec<_> = input.split("\n").collect();
     let mut page_map: HashMap<usize, PageOrder> = HashMap::new();
     let mut answer = 0;
 
     for line in lines {
         if line.contains("|") {
-            // Order declaration
             let order: Vec<_> = line.split("|").map(|f: &str| f.parse::<usize>().expect("Expect a number here")).collect();
             if order.len() != 2 {
                 panic!("Invalid page order definition")
@@ -47,14 +47,54 @@ fn step1(input: &String) {
             page_map.entry(order[1]).and_modify(|page| page.page_before.push(order[0])).or_insert_with(|| PageOrder { page_after: vec![], page_before: vec![order[0]]});
         }
         if line.contains(",") {
-            // Update print
+            let pages_numbers: Vec<_> = line.split(",").map(|f: &str| f.parse::<usize>().expect("Expect a number here")).collect();
+            let correct_order = is_correct_order(&page_map, &pages_numbers);
+
+            if !correct_order {
+                let mut sorted_pages_number: Vec<usize> = pages_numbers.clone();
+                sorted_pages_number.sort_by(|current, next| {
+                    match page_map.get(&current) {
+                        Some(page) => {
+                            if current == next {
+                                return Ordering::Equal;
+                            }
+                            if page.page_before.contains(next) {
+                                return Ordering::Greater;
+                            }
+                            return Ordering::Less;
+                        },
+                        None => {
+                            return Ordering::Equal;
+                        }
+                    }
+                });
+                answer += sorted_pages_number[sorted_pages_number.len() / 2];
+            }
+        }
+    }
+    println!("Step 2 : {}", answer);
+}
+
+fn step1(input: &String) {
+    let lines: Vec<_> = input.split("\n").collect();
+    let mut page_map: HashMap<usize, PageOrder> = HashMap::new();
+    let mut answer = 0;
+
+    for line in lines {
+        if line.contains("|") {
+            let order: Vec<_> = line.split("|").map(|f: &str| f.parse::<usize>().expect("Expect a number here")).collect();
+            if order.len() != 2 {
+                panic!("Invalid page order definition")
+            }
+            page_map.entry(order[0]).and_modify(|page| page.page_after.push(order[1])).or_insert_with(|| PageOrder { page_after: vec![order[1]], page_before: vec![]});
+            page_map.entry(order[1]).and_modify(|page| page.page_before.push(order[0])).or_insert_with(|| PageOrder { page_after: vec![], page_before: vec![order[0]]});
+        }
+        if line.contains(",") {
             let pages_numbers: Vec<_> = line.split(",").map(|f: &str| f.parse::<usize>().expect("Expect a number here")).collect();
             let correct_order = is_correct_order(&page_map, &pages_numbers);
 
             if correct_order {
                 answer += pages_numbers[pages_numbers.len() / 2];
-            } else {
-                // println!("Line {} is in wrong order", line);
             }
         }
     }
@@ -64,4 +104,5 @@ fn step1(input: &String) {
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Unable to read input");
     step1(&input);
+    step2(&input);
 }
