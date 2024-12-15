fd3998b7ab0305938a9346ccab602944668a9c9a
Guillaume Guerin <guerin.dev@gmail.com>
2024-12-15 21:52:56 +0100

    add: day 15 step 2

--- day-15-warehouse-woes/src/main.rs
+++ day-15-warehouse-woes/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn find_robot(grid: &Vec<Vec<u8>>) -> Result<(usize, usize), ()> {
     for (y, line) in grid.into_iter().enumerate() {
@@ -90,6 +90,19 @@ fn compute_score(grid: &Vec<Vec<u8>>) -> usize {
     score
 }
 
+fn compute_score2(grid: &Vec<Vec<u8>>) -> usize {
+    let mut score = 0;
+
+    for (y, line) in grid.into_iter().enumerate() {
+        for (x, c) in line.into_iter().enumerate() {
+            if *c == b'[' {
+                score += 100 * y + x;
+            }
+        }
+    }
+    score
+}
+
 fn step1(input: &String) {
     let binding = input.replace("\r\n", "\n");
     let s: Vec<_> = binding.split("\n\n").collect();
@@ -115,14 +128,103 @@ fn step1(input: &String) {
             '>' => check_next(&mut grid, (robot.0, robot.1 + 1), c, &mut robot),
             _ => {}
         }
-        // display_map(&grid);
     }
 
     println!("Step 1 : {}", compute_score(&grid));
 }
 
+fn add_dir(pos: (usize, usize), dir: char) -> (usize, usize) {
+    match dir {
+        '^' => (pos.0 - 1, pos.1),
+        'v' => (pos.0 + 1, pos.1),
+        '<' => (pos.0, pos.1 - 1),
+        '>' => (pos.0, pos.1 + 1),
+        _ => panic!("Invalid movement"),
+    }
+}
+
+fn expand_frontier(
+    grid: &Vec<Vec<u8>>,
+    pos: (usize, usize),
+    dir: char,
+) -> Option<HashSet<(usize, usize)>> {
+    let mut frontier = Vec::from([pos]);
+    let mut all = HashSet::from([pos]);
+
+    while let Some(curr) = frontier.pop() {
+        let next = add_dir(curr, dir);
+        if all.contains(&next) {
+            continue;
+        }
+        let c = grid[next.0][next.1];
+        match c {
+            b'.' => {}
+            b']' | b'[' => {
+                frontier.push(next);
+                all.insert(next);
+                if dir == '^' || dir == 'v' {
+                    let other = if c == b']' {
+                        (next.0, next.1 - 1)
+                    } else {
+                        (next.0, next.1 + 1)
+                    };
+                    frontier.push(other);
+                    all.insert(other);
+                }
+            }
+            _ => return None,
+        }
+    }
+    Some(all)
+}
+
+// Based on https://github.com/syncd010/AoC2024/blob/main/src/day15.rs answer. Did not manage to make it work on my own
+fn step2(input: &String) {
+    let binding = input
+        .replace("\r\n", "\n")
+        .replace("#", "##")
+        .replace("O", "[]")
+        .replace(".", "..")
+        .replace("@", "@.");
+    let s: Vec<_> = binding.split("\n\n").collect();
+
+    if s.len() != 2 {
+        panic!("Invalid input file")
+    }
+
+    let grid_txt = s[0];
+    let mut grid: Vec<Vec<u8>> = grid_txt
+        .lines()
+        .map(|line| line.bytes().collect())
+        .collect();
+    let mut scratch_grid = grid.clone();
+    let movements = s[1].replace("\n", "");
+
+    let mut robot: (usize, usize) = find_robot(&grid).expect("Expect robot in input");
+
+    for m in movements.chars() {
+        if let Some(frontier) = expand_frontier(&grid, robot, m) {
+            for p in frontier.iter() {
+                scratch_grid[p.0][p.1] = grid[p.0][p.1];
+            }
+            for p in frontier.iter() {
+                grid[p.0][p.1] = b'.';
+            }
+            for p in frontier.iter() {
+                let new_p = add_dir(*p, m);
+                grid[new_p.0][new_p.1] = scratch_grid[p.0][p.1];
+            }
+            robot = add_dir(robot, m);
+        }
+    }
+
+    let answer = compute_score2(&grid);
+
+    println!("Step 2 : {}", answer);
+}
+
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Unable to read input");
     step1(&input);
-    // step2(&input);
+    step2(&input);
 }
